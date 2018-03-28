265b08936aa36ce02412a80d22136325e5073125
Jacob Berman <jacobberman1995@gmail.com>
2018-03-28 15:32:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nestedTarget(){
 function increaseRankBy(n){
   const list = document.querySelectorAll('#ranked-list');
   for (let i = 0; i < list.length; i++){
-    list[i].innerHTML = (n+list[i].innerHTML).toString();
+    list[i].innerHTML = parseInt(list[i].innerHTML).toString();
   }
   return list;
   

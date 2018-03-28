fb362c714e7647f3877fc55ef34c5f2493bba7f5
Jacob Berman <jacobberman1995@gmail.com>
2018-03-28 15:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function nestedTarget(){
   const selector = document.querySelector('#nested .target');
   return selector;
 }
+
+function increaseRankBy(n){
+  document.querySelector(.ranked-list)
+}
